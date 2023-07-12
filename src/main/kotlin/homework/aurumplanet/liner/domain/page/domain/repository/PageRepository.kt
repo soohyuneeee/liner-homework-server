b84c5810a31dfd16ca9cda6889700249b1319355
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 20:37:03 +0900

    {Del} 안쓰는 메서드 제거

--- src/main/kotlin/homework/aurumplanet/liner/domain/page/domain/repository/PageRepository.kt
+++ src/main/kotlin/homework/aurumplanet/liner/domain/page/domain/repository/PageRepository.kt
@@ -10,12 +10,8 @@ import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface PageRepository : JpaRepository<PageEntity, Long> {
-    fun findByUrlAndUser(url: String, user: User): Optional<PageEntity>
-    fun existsByUrlAndUser(url: String, user: User): Boolean
     fun findByUserAndUrl(user: User, url: String): Optional<PageEntity>
-
-    @Query("select p from PageEntity p join fetch p.highlights h where p.user = :user order by h.updatedAt desc")
-    fun findPagesWithHighlightsOrderByUpdatedAtDesc(user: User): List<PageEntity>
+    fun existsByUserAndUrl(user: User, url: String): Boolean
 
     @Query("select p from PageEntity p join fetch p.highlights h where p in (select m.page from Mention m where m.user = :user) or p.openStatus = :openStatus or p.user = :user order by h.createdAt desc")
     fun findPagesWithMentionsOrPublic(user: User, openStatus: OpenStatus, pageable: Pageable): Page<PageEntity>
