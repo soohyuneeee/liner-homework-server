b834c2d0c3155cbb979c6ba0f6bb96d71afc11f3
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-13 00:54:30 +0900

    {Add} deleteByUserAndPage

--- src/main/kotlin/homework/aurumplanet/liner/domain/mention/domain/repository/MentionRepository.kt
+++ src/main/kotlin/homework/aurumplanet/liner/domain/mention/domain/repository/MentionRepository.kt
@@ -1,7 +1,10 @@
 package homework.aurumplanet.liner.domain.mention.domain.repository
 
 import homework.aurumplanet.liner.domain.mention.domain.Mention
+import homework.aurumplanet.liner.domain.page.domain.PageEntity
+import homework.aurumplanet.liner.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MentionRepository : JpaRepository<Mention, Long> {
+    fun deleteByUserAndPage(user: User, page: PageEntity)
 }
