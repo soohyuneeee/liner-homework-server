6131639516848163729aab8daeb3d32fe4b6b321
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 15:40:30 +0900

    {Add} Jwt 토큰 추가

---
+++ src/main/kotlin/homework/aurumplanet/liner/global/config/properties/JwtProperties.kt
@@ -0,0 +1,13 @@
+package homework.aurumplanet.liner.global.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+        val accessTokenValidTime: Long,
+        val refreshTokenValidTime: Long,
+        val prefix: String,
+        val header: String,
+        val secretKey: String,
+)
