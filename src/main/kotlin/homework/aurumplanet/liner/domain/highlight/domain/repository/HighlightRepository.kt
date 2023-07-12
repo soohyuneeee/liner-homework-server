ef87489233d739e2fde957edea71f68629f42afa
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 20:10:49 +0900

    {Del} 필요없는 임포트

--- src/main/kotlin/homework/aurumplanet/liner/domain/highlight/domain/repository/HighlightRepository.kt
+++ src/main/kotlin/homework/aurumplanet/liner/domain/highlight/domain/repository/HighlightRepository.kt
@@ -1,7 +1,6 @@
 package homework.aurumplanet.liner.domain.highlight.domain.repository
 
 import homework.aurumplanet.liner.domain.highlight.domain.Highlight
-import homework.aurumplanet.liner.domain.page.domain.Page
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface HighlightRepository : JpaRepository<Highlight, Long> {
