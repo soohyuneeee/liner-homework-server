ecd2a3974f709bfe448fb1fd8ade314166326dac
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 21:47:46 +0900

    {Fix} 필요없는 필드 제거해서 바로 List<GetPageResponse>

--- src/main/kotlin/homework/aurumplanet/liner/domain/page/presentation/dto/response/GetFeedListResponse.kt
+++ src/main/kotlin/homework/aurumplanet/liner/domain/page/presentation/dto/response/GetFeedListResponse.kt
@@ -3,5 +3,5 @@ package homework.aurumplanet.liner.domain.page.presentation.dto.response
 data class GetFeedListResponse(
     var currentPage: Int,
     val hasMorePage: Boolean,
-    val feedList: List<GetFeedResponse>
+    val feedList: List<GetPageResponse>
 )
