1591400ab84c287dbabda79827ed860952f4a738
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 15:08:09 +0900

    {Add} setting

---
+++ src/main/kotlin/homework/aurumplanet/liner/LinerApplication.kt
@@ -0,0 +1,11 @@
+package homework.aurumplanet.liner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinerApplication>(*args)
+}
---
+++ src/test/kotlin/homework/aurumplanet/liner/LinerApplicationTests.kt
@@ -0,0 +1,13 @@
+package homework.aurumplanet.liner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
