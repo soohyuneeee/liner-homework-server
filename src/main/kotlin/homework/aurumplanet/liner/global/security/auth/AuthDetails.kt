f958e5a7bd9b25d37f6b9c5562afb58c7f9a1bd7
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-12 16:00:02 +0900

    {Add}  authDetail

---
+++ src/main/kotlin/homework/aurumplanet/liner/global/security/auth/AuthDetails.kt
@@ -0,0 +1,39 @@
+package homework.aurumplanet.liner.global.security.auth
+
+import homework.aurumplanet.liner.domain.user.domain.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    val user: User,
+): UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+    }
+
+    override fun getPassword(): String {
+        return this.user.password
+    }
+
+    override fun getUsername(): String {
+        return this.user.nickname
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}
---
+++ src/main/kotlin/homework/aurumplanet/liner/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,16 @@
+package homework.aurumplanet.liner.global.security.auth
+
+import homework.aurumplanet.liner.domain.user.facade.UserFacade
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+@Service
+class AuthDetailsService(
+    val userFacade: UserFacade,
+) : UserDetailsService {
+
+    override fun loadUserByUsername(userId: String): UserDetails {
+        return AuthDetails(userFacade.findUserByUserIdAsString(userId))
+    }
+}
